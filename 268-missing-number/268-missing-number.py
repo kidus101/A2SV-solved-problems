0117811a1bbf309bb1751a6eb21af5f20472c7ac
kidus <69918484+kidus101@users.noreply.github.com>
2022-12-23 19:35:06 +0300

    Time: 182 ms (74.57%), Space: 15.3 MB (43.80%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,35 +1,54 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        _max = max(nums)
-        dic = dict()
-        total_list = list()
-        result = ""
         N = len(nums)
         
-       # [3,0,1]
-        
-        #looping through the max+1 to create the list of total
-        for i in range(N+1):
-            total_list.append(i)
+        return ((N) * (N+1) // 2 - sum(nums))
        
-            
-        for i in range(len(nums)):
-            if (nums[i] in total_list):
-                total_list.remove(nums[i])
-            else:
-                return nums[i]
-        return total_list[0] 
+#         total_list = list()
+#         N = len(nums)
         
-        # TC: O(N) & SC:O(1)
-            
-        # print(dic)
+#        # [3,0,1]
         
-#         for index,value in range(len(total_list)):
+#         #looping through the max+1 to create the list of total
+#         for i in range(N+1):
+#             total_list.append(i)
             
-#             for k in range(len(nums)):
-#                 if nums[k] in total_list
+#         for i in range(len(nums)):
+#             if (nums[i] in total_list):
+#                 total_list.remove(nums[i])
+#             else:
+#                 return nums[i]
+            
+#         return total_list[0] 
         
-        # for j  in range(total_list):
+#         # TC: O(N) & SC:O(1)
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
             
             
             
