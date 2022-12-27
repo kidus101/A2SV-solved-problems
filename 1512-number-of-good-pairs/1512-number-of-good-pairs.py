ecd605f4a981c42b5ac3609a6f05a7ed1d7d4dd2
kidus <69918484+kidus101@users.noreply.github.com>
2022-12-27 05:52:25 +0300

    Time: 35 ms (88.36%), Space: 14 MB (10.58%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.py
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -1,12 +1,30 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        N = len(nums)
+        dictionary = dict()
         counter = 0
-        
-        for i in range(N):
-            for j in range(i+1,N):
-                if nums[i] == nums[j]:
-                    counter += 1
+        [1,2,3,1,1,3]
+
+        for num in nums:
+            if num in dictionary:
+                counter += dictionary[num]
+                dictionary[num] += 1
+                
+            else:
+                dictionary[num] = 1
         return counter
+        
+        # TC:O(N) & sC:O(1)
+        
+        
+        
+        
+#         N = len(nums)
+#         counter = 0
+        
+#         for i in range(N):
+#             for j in range(i+1,N):
+#                 if nums[i] == nums[j]:
+#                     counter += 1
+#         return counter
     
-    #Time complexity = o(n^2)    & SC=O(1)                                                                                                                                                                                                                                                                                                                                                                                                                                              
+#     #Time complexity = o(n^2)    & SC=O(1)                                                                                                                                                                                                                                                                                                                                                                                                                                              
