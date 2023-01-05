2b3f31abc0c120095cd9434b2c6d58945bed6149
kidus <69918484+kidus101@users.noreply.github.com>
2023-01-05 15:16:44 +0300

    Time: 64 ms (20.29%), Space: 13.9 MB (64.35%) - LeetHub

---
+++ 1324-print-words-vertically/1324-print-words-vertically.py
@@ -0,0 +1,26 @@
+class Solution:
+    def printVertically(self, s: str) -> List[str]:
+        
+       
+        string_list = list()
+        string_list =s.split(" ")
+        output_list = list()
+        columns = 0
+        rows = len(string_list)
+        
+        for new_string in string_list:
+            columns = max(columns ,len(new_string))
+       
+
+        for column in range(columns):
+            word = ""
+            for row in range(rows):
+                if column < len(string_list[row]):
+                    word += string_list[row][column]
+                else:
+                    word += " "
+            word=word.rstrip()
+            output_list.append(word)
+        return (output_list)  
+        
+       
