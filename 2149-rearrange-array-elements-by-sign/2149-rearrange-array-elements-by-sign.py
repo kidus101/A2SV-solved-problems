d87df78edf189ffbb2f3e724579685f67b7b697c
kidus <69918484+kidus101@users.noreply.github.com>
2023-01-10 23:03:59 +0300

    Time: 1669 ms (64.62%), Space: 44.8 MB (62.99%) - LeetHub

---
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.py
@@ -0,0 +1,28 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        
+        negative_list = list()
+        postive_list = list()
+        final_list = list()
+        output = list()
+        
+        ptr1 = 0
+        ptr2 = len(nums) // 2
+        
+        for num in nums:
+            if num < 0 :
+                negative_list.append(num)
+            if num >= 0:
+                postive_list.append(num)
+        final_list = postive_list+ negative_list
+        # print(final_list)
+        # print(len(postive_list))
+
+        while ptr1 < len(postive_list) or ptr2 < len(final_list):
+            output.append(final_list[ptr1])
+            output.append(final_list[ptr2])
+            ptr1 += 1
+            ptr2 += 1
+        return (output)
+    
+    # Time: O(N) & Space:O(N)
