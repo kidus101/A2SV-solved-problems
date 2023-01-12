83158709109427b0b2a5cd8ed7895786aaa48ba5
kidus <69918484+kidus101@users.noreply.github.com>
2023-01-12 16:30:55 +0300

    Time: 178 ms (78.50%), Space: 14.8 MB (10.11%) - LeetHub

---
+++ 944-delete-columns-to-make-sorted/944-delete-columns-to-make-sorted.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minDeletionSize(self, strs: List[str]) -> int:
+        sub_list = list()
+        
+        
+            
+        for cols in range(len(strs[1])):
+            string = ""
+            for rows in range(len(strs)):
+                string += strs[rows][cols]
+                
+            sub_list.append(string)
+            
+        count = 0   
+        sorted_word = ""
+        
+        for word in sub_list:
+            sorted_string = sorted(word)
+            joined_string = "".join(sorted_string)
+            if word != joined_string:
+                print(word)
+                print(joined_string)
+                
+                count += 1
+        return count
+    
+    # Time:O(N) and Space:O(1)
