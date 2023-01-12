7a0d23ec67841173421dd8a25830ae108e5796d3
kidus <69918484+kidus101@users.noreply.github.com>
2023-01-12 16:08:50 +0300

    Time: 55 ms (86.04%), Space: 14.2 MB (60.56%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -6,17 +6,12 @@ class Solution:
         
         i = 0
         
-        
-        output =list() 
-        
         while i < len(nums)-1:
             if nums[i] == nums[i+1]:
                 nums[i] *= 2
                 nums[i+1] = 0
             i += 1
-           
-        
-        # print(nums)
+
             
         left = 0
         right = 1
