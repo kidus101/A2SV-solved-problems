a5fc272ff8ce14d90f4d6f5d9bee38bb81ec9850
kidus <69918484+kidus101@users.noreply.github.com>
2023-01-12 16:08:02 +0300

    Time: 64 ms (68.10%), Space: 14.1 MB (60.56%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -0,0 +1,41 @@
+class Solution:
+    def applyOperations(self, nums: List[int]) -> List[int]:
+        #check if nums[i] == nums[i+1] 
+        # if yes do operations else both incerement by 1
+        #After the array is finished we move the zeros to the last 
+        
+        i = 0
+        
+        
+        output =list() 
+        
+        while i < len(nums)-1:
+            if nums[i] == nums[i+1]:
+                nums[i] *= 2
+                nums[i+1] = 0
+            i += 1
+           
+        
+        # print(nums)
+            
+        left = 0
+        right = 1
+        
+        while right < len(nums):
+            if nums[left] != 0:
+                left += 1
+                right +=1
+            elif nums[right] == 0:
+                right +=1
+            else:
+                temp = nums[left]
+                nums[left]=nums[right]
+                nums[right] = temp
+                left +=1
+                right+=1
+
+        return nums
+    
+    # Time:O(N) & Space:O(1)
+      
+            
