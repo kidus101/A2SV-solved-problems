ccb25c298de3c281d453622f682777e09a7fcd45
kidus <69918484+kidus101@users.noreply.github.com>
2023-01-21 21:42:43 +0300

    Time: 999 ms (45.04%), Space: 39.3 MB (85.10%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.py
@@ -1,27 +1,38 @@
+# class Solution:
+#     def countDistinctIntegers(self, nums: List[int]) -> int:
+        
+#         # Time:O(N) & SC:O(N)
+#         string_list = list()    
+#         tot_list = set()
+#         final_list = set()
+# #      Changing the int list to str list
+#         for num in nums:
+#             string_list.append(str(num))
+# #    creating a copy of string_list     
+#         for num in nums:
+#             tot_list.add(str(num))
+# # reversing the sting and adding to a set     
+#         for num in string_list:
+#             tot_list.add(num[::-1])
+# #   changing back to int to deal with "01 "== "10 " Problem    
+#         for num in tot_list:
+#             final_list.add(int(num))
+        
+#         return len(final_list)
+            
+#      OPTIMIZATION
+
 class Solution:
     def countDistinctIntegers(self, nums: List[int]) -> int:
-
-        # Time:O(N) & SC:O(N)
-        string_list = list()    
-        tot_list = set()
-        final_list = set()
-#      Changing the int list to str list
-        for num in nums:
-            string_list.append(str(num))
-#    creating a copy of string_list     
-        for num in nums:
-            tot_list.add(str(num))
-# reversing the sting and adding to a set     
-        for num in string_list:
-            tot_list.add(num[::-1])
-#   changing back to int to deal with "01 "== "10 " Problem    
-        for num in tot_list:
-            final_list.add(int(num))
         
-        return len(final_list)
-            
-            
-            
+        set_nums = set(nums)
+        
+        for num in nums:
+            str_num = str(num)
+            reversed_num = str_num[::-1]
+            set_nums.add(int(reversed_num))
+        
+        return len(set_nums)
             
             
             
