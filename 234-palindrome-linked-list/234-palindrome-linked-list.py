9bafb931fd681dce9633597701c9fc6e36915425
kidus <69918484+kidus101@users.noreply.github.com>
2023-02-21 12:05:15 +0300

    Time: 780 ms (75.97%), Space: 39.1 MB (66.45%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.py
+++ 234-palindrome-linked-list/234-palindrome-linked-list.py
@@ -32,67 +32,7 @@ class Solution:
         
         
         
-        # left = head
-        # right = prev
         
-        # while left:
-        #     # print(head)
-        #     print(right)
-        #     if left.val == right.val:
-        #         left = left.next
-        #         right = right.next
-        #     else:
-        #         return False
-        # return True
-
-            
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         #USING o(1) SPACE COMPLEXITY AND A LINKED LIST
-        
-#         fast = head 
-#         slow = head
-        
-#         while fast and fast . next:
-#             fast = fast.next.next
-#             slow= slow.next
-            
-#         prev = None
-#         while slow:
-#             temp = slow.next
-#             slow . next = prev
-#             prev = slow
-#             slow = temp
-            
-#         left , right =head , prev
-#         while right :
-#             if left.val != right.val:
-#                 return False
-#             left = left.next
-#             right = right.next
-#         return True
             
         
         #Time Complexity : O(n)
