61d684e9db507a109f1796714d4c89b2d8820494
kidus <69918484+kidus101@users.noreply.github.com>
2023-02-28 11:05:17 +0300

    Time: 31 ms (77.33%), Space: 13.7 MB (93.56%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    @lru_cache(None)
+    def fib(self, n: int) -> int:
+         # Time:O(n) # Space:O(n)
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        return self.fib(n-1) + self.fib(n-2)
+    
+   
