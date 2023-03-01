0c189fcce5c483d7ef96f342257d459a9d58c442
kidus <69918484+kidus101@users.noreply.github.com>
2023-03-01 10:42:27 +0300

    Time: 210 ms (42.38%), Space: 43.3 MB (5.48%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -1,18 +1,49 @@
 class Solution:
     def reverseString(self, s: List[str]) -> None:
-        
-        """
-        Do not return anything, modify s in-place instead.
-        """
+# TC:O(N) & SC:O(N)
+
+#         Using Recurssion Method 2
+
         left = 0
-        right = len(s)-1
-        # temp = int()
+        right = len(s) - 1
         
-        while left < right :
+        def helper(left , right):
+            
+            if left >= right:
+                return
+        
             s[left] , s[right] = s[right] , s[left]
+
+            return helper(left+1 , right-1)   
+        
+        helper(left,right)
+        
+        
+        
+#         Using Recurssion Method 1
+
+#         left = 0
+#         right = len(s) - 1
+        
+#         if left >= right:
+#             return
+        
+#         s[left] , s[right] = s[right] , s[left]
+#         left += 1
+#         right -= 1
+        
+#         return self.reverseString(s[left:right+1])
+
+        
+#         left = 0
+#         right = len(s)-1
+#         # temp = int()
+        
+#         while left < right :
+#             s[left] , s[right] = s[right] , s[left]
                
-            left += 1
-            right -= 1
+#             left += 1
+#             right -= 1
             
             # temp = s[left]
             # s[left]=s[right] 
