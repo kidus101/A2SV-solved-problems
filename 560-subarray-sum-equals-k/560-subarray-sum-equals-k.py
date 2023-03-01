72a638a427753f3ca886eabc890b31e2350d7ac6
kidus <69918484+kidus101@users.noreply.github.com>
2023-03-01 17:15:54 +0300

    Time: 288 ms (96.19%), Space: 16.6 MB (30.16%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        
+        dict ={0 : 1}
+        n = len(nums)
+        count = 0
+        preSum = 0
+        
+        for num in nums:
+            preSum += num
+            
+            if preSum -k in dict:
+                count += dict[preSum-k]
+                
+            if preSum in dict:
+                dict[preSum] += 1
+                
+            else:
+                dict[preSum] = 1
+        
+        return count
+        
+        # Time:O(n)
+        # Space:O(n)
