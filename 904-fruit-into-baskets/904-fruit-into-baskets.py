8ede264a2da18adf88d72c651c6373edcc42b7b3
kidus <69918484+kidus101@users.noreply.github.com>
2023-03-02 16:33:58 +0300

    Time: 937 ms (63.85%), Space: 20.5 MB (29.03%) - LeetHub

---
+++ 904-fruit-into-baskets/904-fruit-into-baskets.py
@@ -0,0 +1,66 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        # TC:O(N) & SC:O(N)
+        
+        basket_dict = defaultdict(int)
+        
+        max_count = 0
+        left = 0
+        right = 0
+        
+        for right in range(len(fruits)):
+            
+            basket_dict[fruits[right]] += 1
+            
+            while len(basket_dict) > 2:
+                basket_dict[fruits[left]] -= 1
+                
+                if basket_dict[fruits[left]] == 0:
+                    basket_dict.pop(fruits[left])
+                    
+                left += 1
+                
+            
+            
+        
+    
+            max_count = max(max_count , right-left+1)
+                
+        max_count = max(max_count , right-left+1) 
+            
+        return max_count
+            
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         start, end = 0 , 0
+#         max_len =0
+#         d={}
+        
+#         while end < len(tree):
+#             d[tree[end]] = end
+            
+#             if len(d) >= 3:
+#                 min_val = min(d.values())
+#                 del d[tree[min_val]]
+#                 start = min_val +1
+#             max_len = max(max_len, end - start + 1)
+#             end += 1
+#         return max_len
+    
+    #Time complexity :O(n)
+    #Space Complexity: O(1)
