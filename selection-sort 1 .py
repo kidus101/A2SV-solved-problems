084ba4c4cece60ab02c8cd7dd86748d18e5c608f
kidus <69918484+kidus101@users.noreply.github.com>
2023-03-24 14:48:22 +0300

    selection sort Hacker rank solution

---
+++ selection-sort 1 .py
@@ -0,0 +1,17 @@
+
+class Solution: 
+
+    def selectionSort(self,arr,n):
+        
+        for i in range(len(arr)):
+
+            min_index = i
+            for j in range(i+1, len(arr)):
+                if arr[j] < arr[min_index]:
+                    min_index = j
+            
+
+            arr[i], arr[min_index] = arr[min_index], arr[i]
+        
+    
+        return arr
