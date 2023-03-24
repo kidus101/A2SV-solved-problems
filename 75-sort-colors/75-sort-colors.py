d766d65751ac2e487eff3eb038b905fa36efeaa3
kidus <69918484+kidus101@users.noreply.github.com>
2023-03-24 14:10:22 +0300

    Time: 39 ms (38.85%), Space: 13.8 MB (51.99%) - LeetHub

--- 75-sort-colors/75-sort-colors.py
+++ 75-sort-colors/75-sort-colors.py
@@ -1,3 +1,27 @@
+def counting_sort(nums):
+    """
+    Sorts the given list of integers in ascending order using counting sort.
+    """
+    max_num = max(nums)
+    count = [0] * (max_num + 1)
+    sorted_nums = [0] * len(nums)
+    
+    # Count the frequency of each number in the list
+    for num in nums:
+        count[num] += 1
+    
+    # Calculate the cumulative sum of the count array
+    for i in range(1, len(count)):
+        count[i] += count[i-1]
+    
+    # Sort the list in ascending order
+    for num in nums:
+        index = count[num] - 1
+        sorted_nums[index] = num
+        count[num] -= 1
+    
+    return sorted_nums
+
 
   # Using Counting Sort  
 # 1. knowing the max number
@@ -5,6 +29,26 @@
 # 3. Counting in the respective places of the count arra
 # 4. Finding the cumulative sum of coun and updating count
 # 5.
+
+
+
+
+
+
+# def sortColors(self, nums):
+#     red, white, blue = 0, 0, len(nums)-1
+    
+#     while white <= blue:
+#         if nums[white] == 0:
+#             nums[red], nums[white] = nums[white], nums[red]
+#             white += 1
+#             red += 1
+#         elif nums[white] == 1:
+#             white += 1
+#         else:
+#             nums[white], nums[blue] = nums[blue], nums[white]
+#             blue -= 1
+
 
 class Solution:
     def sortColors(self, array: List[int]) -> None:
