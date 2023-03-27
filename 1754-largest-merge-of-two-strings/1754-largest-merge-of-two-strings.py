9077c2c2a570d47d2f5ba84d7687ab2056e854ef
kidus <69918484+kidus101@users.noreply.github.com>
2023-03-27 23:04:10 +0300

    Time: 98 ms (64.14%), Space: 14.3 MB (13.89%) - LeetHub

---
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+	def largestMerge(self, word1: str, word2: str) -> str:
+		list1 = list(word1)
+		list2 = list(word2)
+
+		output = []
+
+		while list1 and list2:
+			if list1 > list2:
+				output.append(list1[0])
+				list1.pop(0)
+			else:
+				output.append(list2[0])
+				list2.pop(0)
+
+		return "".join(output + list1 + list2)
