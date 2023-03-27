3f76ebd75b2e07367b989859c0c512192bcb5d8b
kidus <69918484+kidus101@users.noreply.github.com>
2023-03-27 19:20:04 +0300

    Time: 4250 ms (5.03%), Space: 15.4 MB (16.10%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,27 +1,67 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        N = len(nums)
+        output = [ i for i in range(len(nums) + 1)]
+
+        for num in nums:
+            if num in output:
+                output.remove(num)
+            else:
+                continue
+
+        return  output[0]
+
         
-        return ((N) * (N+1) // 2 - sum(nums))
-       
-#         total_list = list()
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
 #         N = len(nums)
         
-#        # [3,0,1]
+#         return ((N) * (N+1) // 2 - sum(nums))
+       
+# #         total_list = list()
+# #         N = len(nums)
         
-#         #looping through the max+1 to create the list of total
-#         for i in range(N+1):
-#             total_list.append(i)
-            
-#         for i in range(len(nums)):
-#             if (nums[i] in total_list):
-#                 total_list.remove(nums[i])
-#             else:
-#                 return nums[i]
-            
-#         return total_list[0] 
+# #        # [3,0,1]
         
-#         # TC: O(N) & SC:O(1)
+# #         #looping through the max+1 to create the list of total
+# #         for i in range(N+1):
+# #             total_list.append(i)
+            
+# #         for i in range(len(nums)):
+# #             if (nums[i] in total_list):
+# #                 total_list.remove(nums[i])
+# #             else:
+# #                 return nums[i]
+            
+# #         return total_list[0] 
+        
+# #         # TC: O(N) & SC:O(1)
             
         
         
