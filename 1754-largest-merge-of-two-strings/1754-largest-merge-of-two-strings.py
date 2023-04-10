430852d35e784c893ad6e1544c1fea517a3b7479
kidus <69918484+kidus101@users.noreply.github.com>
2023-04-10 22:04:03 +0300

    Time: 97 ms (61.68%), Space: 14.2 MB (15.69%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
@@ -4,13 +4,16 @@ class Solution:
 		list2 = list(word2)
 
 		output = []
-
+        # print(list1)
+        
 		while list1 and list2:
+            
 			if list1 > list2:
 				output.append(list1[0])
 				list1.pop(0)
+                
 			else:
 				output.append(list2[0])
 				list2.pop(0)
 
-		return "".join(output + list1 + list2)
+		return "".join( output + list1 + list2 )
