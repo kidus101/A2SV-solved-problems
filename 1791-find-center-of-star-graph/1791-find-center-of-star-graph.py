3de519a5052aad5fcc27ad0e773e61b993a616a5
kidus <69918484+kidus101@users.noreply.github.com>
2023-04-11 22:34:52 +0300

    Time: 820 ms (59.76%), Space: 50.1 MB (22.25%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.py
@@ -1,6 +1,15 @@
 class Solution:
     def findCenter(self, edges: List[List[int]]) -> int:
-        return list(set(edges[0]) .intersection(edges[1] ))[0]
+        if len(edges) == 1:
+            return edges[0][0]
+        elif edges[0][0] == edges[1][0] or edges[0][0] == edges[1][1]:
+            return edges[0][0]
+        else:
+            return edges[0][1]
+
+# class Solution:
+#     def findCenter(self, edges: List[List[int]]) -> int:
+#         return list(set(edges[0]) .intersection(edges[1] ))[0]
        
             
         
