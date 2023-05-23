f0252d92419256c5da0962394af20998421ea1bd
kidus <69918484+kidus101@users.noreply.github.com>
2023-05-23 21:38:26 +0300

    Time: 1562 ms (67.84%), Space: 83.9 MB (65.30%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,52 @@
+class Solution:
+    class UnionFind:
+        def __init__(self, n):
+            self.parent = list(range(n))
+            self.rank = [0] * n
+
+        def find(self, x):
+            if self.parent[x] != x:
+                self.parent[x] = self.find(self.parent[x])
+            return self.parent[x]
+
+        def union(self, x, y):
+            root_x = self.find(x)
+            root_y = self.find(y)
+
+            if root_x == root_y:
+                return False
+
+            if self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+            elif self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+            else:
+                self.parent[root_x] = root_y
+                self.rank[root_y] += 1
+
+            return True
+
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        edges = []
+
+        for i in range(n):
+            for j in range(i + 1, n):
+                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                edges.append((dist, i, j))
+
+        edges.sort()
+
+        min_cost = 0
+        uf = self.UnionFind(n)
+        num_edges = 0
+
+        for dist, i, j in edges:
+            if uf.union(i, j):
+                min_cost += dist
+                num_edges += 1
+
+            if num_edges == n - 1:
+                break
+
+        return min_cost
