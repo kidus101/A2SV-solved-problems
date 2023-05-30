6cd70860419395332f7cbfc8f8fc7178bc3279b7
kidus <69918484+kidus101@users.noreply.github.com>
2023-05-30 18:11:15 +0300

    Time: 48 ms (22.45%), Space: 16.3 MB (9.97%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,31 +1,88 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
+        one , two = 1 , 1
         
-        memo = defaultdict(int)
-        def fib(self,n):
+        for i in range(n-1):
+            temp = one
+            one = one + two
+            two = temp
+        
+        return one
+        
+        
+#         dict_ = { 1:1 , 2:2 } 
+        
+#         if n <= 2:
+#             return n
+        
+#         if n in dict_:
+#             return dict_[n]
+        
+#         k = self.climbStairs(n-1) + self.climbStairs(n-2)
+        
+#         dict_[n] = k
+        
+#         return dict_[n]
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+#         memo = defaultdict(int)
+#         def fib(self,n):
             
-            result = 0
-            if n <= 2:
-                return n
+#             result = 0
+#             if n <= 2:
+#                 return n
             
-            print(memo[n-1])
+#             print(memo[n-1])
             
-            if memo[n-1]:
-                result += memo[n-1]
+#             if memo[n-1]:
+#                 result += memo[n-1]
                 
-            else:
-                del memo[n-1]
-                result += fib(self,n-1)
+#             else:
+#                 del memo[n-1]
+#                 result += fib(self,n-1)
                 
-            if memo[n-2]:
-                result += memo[n-2]
+#             if memo[n-2]:
+#                 result += memo[n-2]
                 
-            else:
-                del memo[n-2]
-                result += fib(self,n-2)
+#             else:
+#                 del memo[n-2]
+#                 result += fib(self,n-2)
             
-            memo[n] = result
+#             memo[n] = result
             
-            return result
+#             return result
             
-        return fib(self,n)
+#         return fib(self,n)
